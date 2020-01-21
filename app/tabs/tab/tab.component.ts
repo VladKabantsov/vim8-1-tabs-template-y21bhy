ccd2839cd23bf172c49b3f588231b8551ba0757c
VladKabantsov <kabantsovlad@gmail.com>
2020-01-21 17:59:39 +0300

    WIP bonus task

--- app/tabs/tab/tab.component.ts
+++ app/tabs/tab/tab.component.ts
@@ -1,12 +1,16 @@
-import { Component, ElementRef, ViewChild, Input } from '@angular/core';
+import { Component, ElementRef, ViewChild, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'tab',
   templateUrl: './tab.component.html',
   styleUrls: ['./tab.component.css']
 })
-export class TabComponent {
-  @ViewChild('tab', { static: true, read: ElementRef }) tab;
+export class TabComponent implements OnInit {
+  @ViewChild('tab', { static: false, read: ElementRef }) tab;
   @Input() selectedTab: boolean;
   constructor() { }
+
+  ngOnInit() {
+    console.log(this.tab)
+  }
 }
